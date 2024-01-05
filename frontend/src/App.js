cb81e0e53507e240ca9ad92a75f9a1b24ca93b65
Connor Park <connorpark024@gmail.com>
2024-01-05 16:19:26 -0500

    add helper functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,19 @@ import axios from "axios";
 function App() {
   const [ticker1, setTicker1] = useState("");
   const [ticker2, setTicker2] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [std, setStd] = useState(1.5);
+  const [movingAverageLength, setMovingAverageLength] = useState("");
   const [plots, setPlots] = useState({});
   const [loading, setLoading] = useState(false);
 
   const plotTitles = {
     prices: "Prices",
-    spread: "Stock Price Spread",
-    ratio: "Stock Price Ratio",
+    static: "Stock Price Ratio",
     bands: "Upper and Lower Bands",
-    returns: "Cumulative Returns",
+    static_returns: "Cumulative Returns",
+    bands_returns: "Cumulative Returns",
   };
 
   const handleSubmit = async (event) => {
@@ -23,8 +27,13 @@ function App() {
       const response = await axios.post("http://localhost:8000/api/stocks/", {
         ticker1,
         ticker2,
+        startDate,
+        endDate,
+        std: parseFloat(std),
+        window: parseInt(movingAverageLength),
       });
       setPlots(response.data);
+      console.log(response.data);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -33,25 +42,134 @@ function App() {
   };
 
   return (
-    <div className="App container mx-auto p-4">
-      <form onSubmit={handleSubmit} className="mb-8">
-        {/* ... (your form code) */}
+    <div className="flex flex-col gap-2 container mx-auto p-4">
+      <h1 className="text-4xl text-center">Pairs Trading Algorithm</h1>
+      <h1 className="text-xl text-center">Description</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-4 mx-auto"
+      >
+        <div className="flex gap-4">
+          <div className="flex flex-col">
+            <label htmlFor="ticker1" className="text-lg text-gray-700">
+              Ticker 1
+            </label>
+            <input
+              type="text"
+              id="ticker1"
+              value={ticker1}
+              onChange={(e) => setTicker1(e.target.value)}
+              placeholder="AAPL"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="ticker2" className="text-lg text-gray-700">
+              Ticker 2
+            </label>
+            <input
+              type="text"
+              id="ticker2"
+              value={ticker2}
+              onChange={(e) => setTicker2(e.target.value)}
+              placeholder="MSFT"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="startDate" className="text-lg text-gray-700">
+              Start Date
+            </label>
+            <input
+              type="text"
+              id="startDate"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              placeholder="YYYY-MM-DD"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="endDate" className="text-lg text-gray-700">
+              End Date
+            </label>
+            <input
+              type="text"
+              id="endDate"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              placeholder="YYYY-MM-DD"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="std" className="text-lg text-gray-700">
+              Standard Deviation
+            </label>
+            <input
+              type="number"
+              id="std"
+              value={std}
+              onChange={(e) => setStd(e.target.value)}
+              placeholder="2"
+              step="0.1"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label
+              htmlFor="movingAverageLength"
+              className="text-lg text-gray-700"
+            >
+              Moving Average Length
+            </label>
+            <input
+              type="number"
+              id="movingAverageLength"
+              value={movingAverageLength}
+              onChange={(e) => setMovingAverageLength(e.target.value)}
+              placeholder="20"
+              className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-2/5"
+        >
+          Get Graph
+        </button>
       </form>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="grid grid-cols-2 gap-4">
-          {Object.keys(plots).map((key) => (
-            <div key={key} className="flex flex-col items-center gap-2 mb-8">
-              <h3 className="text-2xl font-bold">{plotTitles[key]}</h3>
-              <img
-                src={`data:image/png;base64,${plots[key]}`}
-                alt={plotTitles[key]}
-                className="max-w-full"
-                style={{ width: "100%" }} // Automatically fit images
-              />
-            </div>
-          ))}
+        <div className="flex flex-col">
+          <div className="flex w-full">
+            <img
+              src={`data:image/png;base64,${plots.prices}`}
+              alt={plotTitles.prices}
+              className="w-3/5 mx-auto"
+            />
+          </div>
+          <div className="grid grid-cols-2">
+            {["static", "bands", "static_returns", "bands_returns"].map(
+              (key) => (
+                <div key={key} className="flex flex-col items-center mb-8">
+                  <img
+                    src={`data:image/png;base64,${plots[key]}`}
+                    alt={plotTitles[key]}
+                    className="max-w-full"
+                    style={{ width: "100%" }}
+                  />
+                </div>
+              )
+            )}
+          </div>
         </div>
       )}
     </div>
--- server/stockapp/views.py
+++ server/stockapp/views.py
@@ -11,163 +11,147 @@ import seaborn as sns
 import io
 import base64
 
-@api_view(['POST'])
-def stock_api(request):
-    matplotlib.use('Agg')
-    plt.rcParams['font.family'] = 'sans-serif'
-    plt.rcParams['font.sans-serif'] = 'Helvetica'
+matplotlib.use('Agg')
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Helvetica'
 
-    ticker1 = request.data.get('ticker1') or 'AAPL' 
-    ticker2 = request.data.get('ticker2') or 'MSFT' 
+def plot_to_base64(plt_figure, dpi=150):
+    """Converts a matplotlib plot to a base64 encoded string."""
+    buffer = io.BytesIO()
+    plt_figure.savefig(buffer, format='png', dpi=dpi)
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
+    return base64.b64encode(image_png).decode('utf-8')
 
-    datetime_start=dt.datetime(2022, 1, 8, 7, 35, 51)
-    datetime_end=dt.datetime.today()    
-    timestamp_start=int(datetime_start.timestamp()) 
-    timestamp_end=int(datetime_end.timestamp()) 
+def download_stock_data(ticker,start_date_str,end_date_str):
+    try:
+        datetime_start = dt.datetime.strptime(start_date_str, '%Y-%m-%d')
+    except ValueError:
+        datetime_start = dt.datetime(2022, 1, 8, 7, 35, 51) 
 
-    def download_stock_data(ticker,timestamp_start,timestamp_end):
-        url=f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={timestamp_start}&period2={timestamp_end}&interval\=1d&events=history&includeAdjustedClose=true"
-        df = pd.read_csv(url)
-        return df
+    try:
+        datetime_end = dt.datetime.strptime(end_date_str, '%Y-%m-%d')
+    except ValueError:
+        datetime_end = dt.datetime.today() - dt.timedelta(days=365)
 
-    # def find_most_correlated_stocks(df):
-    #     corr_matrix = df.corr()
-    #     corr_matrix = corr_matrix.unstack()
-    #     sorted_corr = corr_matrix.sort_values(kind="quicksort", ascending=False)
-    #     most_correlated_pair = sorted_corr.index[sorted_corr != 1][0]
-    #     return most_correlated_pair
+    # Convert datetime objects to timestamps
+    timestamp_start = int(datetime_start.timestamp())
+    timestamp_end = int(datetime_end.timestamp())
+    url=f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={timestamp_start}&period2={timestamp_end}&interval\=1d&events=history&includeAdjustedClose=true"
+    df = pd.read_csv(url)
+    return df
 
-    # # show correlation heatmap
-    # corr = df_global.corr()
-    # plt.figure(figsize=(16, 7))
-    # sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
-    # plt.title('Correlation Matrix Heatmap of Selected Stocks')
-    # heatmap_base64 = plot_to_base64(plt)
-    
-    def plot_to_base64(plt_figure, dpi=150):
-        """Converts a matplotlib plot to a base64 encoded string."""
-        buffer = io.BytesIO()
-        plt_figure.savefig(buffer, format='png', dpi=dpi)
-        buffer.seek(0)
-        image_png = buffer.getvalue()
-        buffer.close()
-        return base64.b64encode(image_png).decode('utf-8')
+def coint_testing(df_stocks, ticker_long, ticker_short):
+    result = ts.coint(df_stocks[ticker_long], df_stocks[ticker_short])
+    coint_p = result[1]
+    adf_p = adfuller(df_stocks[ticker_long] - df_stocks[ticker_short])[1]
+    return coint_p, adf_p
 
-    tickers = [
-    "AAPL",
-    "MSFT",
-    "AMZN",
-    "NVDA",
-    "GOOGL",
-    "META",
-    "TSLA",
-    "BRK-B",
-    "AVGO",
-    "UNH",
-    "JPM",
-    "LLY",
-    "V",
-    "XOM",
-    "JNJ",
-    "HD",
-    "MA",
-    "PG",
-    "COST",
-    "ABBV",
-    "MRK",
-    "ADBE",
-    "CVX",
-    "CRM"
-    ]
-
-    df_global = pd.DataFrame()
-    for ticker in tickers:
-        df_temp = download_stock_data(ticker, timestamp_start, timestamp_end)[['Date', 'Adj Close']]
-        df_temp = df_temp.set_index('Date')
-        df_temp.columns = [ticker]
-        df_global = pd.concat((df_global, df_temp), axis=1)
-    
-    ticker_long = ticker1
-    ticker_short = ticker2
-
-    # cointegration testing
-    result = ts.coint(df_global[ticker_long], df_global[ticker_short])
-    p_val = result[1]
-    print('P value for the augmented Engle-Granger two-step cointegration test is', p_val)
-
-    # adfuller testing
-    spread_adf = adfuller(df_global[ticker_long] - df_global[ticker_short])
-    print('P value for the Augmented Dickey-Fuller Test is', spread_adf[1])
-    ratio_adf = adfuller(df_global[ticker_long] - df_global[ticker_short])
-    print('P value for the Augmented Dickey-Fuller Test is', ratio_adf[1])
-
-    # get spread and ratio between two stocks:
-    spread = df_global[ticker_long] - df_global[ticker_short]
-    ratio = df_global[ticker_long] / df_global[ticker_short]
-
-    # get rolling mean and sd of spread
-    window = 200
-    n_std = 2
-    rolling_mean = ratio.rolling(window=window).mean()
-    rolling_std = ratio.rolling(window=window).std()
-
-    # get z-score of spread and lower and upper bands
-    zscore = (ratio - rolling_mean) / rolling_std
-    upper_band = rolling_mean + n_std * rolling_std
-    lower_band = rolling_mean - n_std * rolling_std
-
-    # plot stock prices
-    figs=(8,4)
-    plt.figure(figsize = figs)
-    df_global[ticker_long].plot(label=ticker_long+'_price')
-    df_global[ticker_short].plot(label=ticker_short+'_price')
+def plot_stock_prices(df_stocks, ticker_long, ticker_short):
+    plt.figure(figsize = (10, 6))
+    df_stocks[ticker_long].plot(label=ticker_long+' Price')
+    df_stocks[ticker_short].plot(label=ticker_short+' Price')
     plt.title("Prices of {0} and {1}".format(ticker_long,ticker_short))
     plt.legend()
-    prices_base64 = plot_to_base64(plt)
+    return plot_to_base64(plt)
 
-    # plot spread
-    figs=(8,4)
-    plt.figure(figsize = figs)
+def plot_spread(df_stocks, ticker_long, ticker_short):
+    spread = df_stocks[ticker_long] - df_stocks[ticker_short]
+    plt.figure(figsize = (8,5))
     spread.plot(label='Spread = '+ticker_long+' - '+ ticker_short,linestyle='--')
     plt.title("Spread of Prices")
     plt.legend()
-    spread_base64 = plot_to_base64(plt)
+    return plot_to_base64(plt)
 
-    # plot ratio
-    figs=(8,4)
-    plt.figure(figsize = figs)
+def plot_ratio_static(ratio, ticker_long, ticker_short, n_std, ratio_mean, ratio_std):
+    plt.figure(figsize = (8,5))
     ratio.plot(label='Ratio = '+ticker_long+' / '+ ticker_short,linestyle='-')
-    plt.title("Ratio of Prices")
+    plt.axhline(ratio_mean - n_std * ratio_std, color='green')
+    plt.axhline(ratio_mean + n_std * ratio_std, color='green')
+    plt.axhline(ratio_mean, color='red')
+    plt.title("Price Ratio of " + ticker_long + " and " + ticker_short + " (Bands)")
     plt.legend()
-    ratio_base64 = plot_to_base64(plt)
+    return plot_to_base64(plt)
 
-    # plot lower and upper bands with ratio and ma
-    plt.figure(figsize = figs)
+def plot_ratio_bollinger(df_stocks, ratio, ticker_long, ticker_short, n_std, ratio_rolling_mean, ratio_rolling_std, window):
+    upper_band = ratio_rolling_mean + n_std * ratio_rolling_std
+    lower_band = ratio_rolling_mean - n_std * ratio_rolling_std
+    plt.figure(figsize = (8,5))
     upper_band.plot(label='Upper_band')
     lower_band.plot(label='Lower_band')
     ratio.plot(label = 'Ratio = '+ticker_long+' - '+ ticker_short,linestyle='--', color='r')
-    rolling_mean.plot(label = 'ma_20days_ratio', linestyle = '-.')
-    plt.fill_between(df_global.index,lower_band, upper_band, alpha=0.2)
+    ratio_rolling_mean.plot(label = 'MA ' + str(window) + ' Ratio', linestyle = '-.')
+    plt.fill_between(df_stocks.index,lower_band, upper_band, alpha=0.2)
+    plt.title("Price Ratio of " + ticker_long + " and " + ticker_short + " (Static)")
     plt.legend()
-    bands_base64 = plot_to_base64(plt)
+    return plot_to_base64(plt)
 
+def plot_returns(df_stocks, ticker_long, ticker_short, zscore, n_std):
     # enter long position if z-score is less than -n_std
     # enter short position if z-score is greater than n_std
     signal = np.where(zscore < -n_std, 1, np.where(zscore > n_std, -1, 0))
-    signal = pd.Series(signal, index=df_global.index)
-
-    # get daily returns
-    returns = df_global[ticker_long].pct_change() - df_global[ticker_short].pct_change()
-
-    # get strategy returns
-    strategy_returns = signal.shift(1) * returns
+    signal = pd.Series(signal, index=df_stocks.index)
 
     # get and plot cumulative returns
+    returns = df_stocks[ticker_long].pct_change() - df_stocks[ticker_short].pct_change()
+    strategy_returns = signal.shift(1) * returns
     cumulative_returns = (1 + strategy_returns).cumprod()
-    plt.figure(figsize=figs)  # Start a new figure
+    plt.figure(figsize=(8,5))
     cumulative_returns.plot()
-    plt.title("Cumulative Return with n_std={0}".format(n_std))
-    returns_base64 = plot_to_base64(plt)  # Encode the plot
-    plt.close()  # Close the plot context
+    plt.title("Cumulative Returns")
+    return plot_to_base64(plt) 
+    
+@api_view(['POST'])
+def stock_api(request):
+    # plot formatting
+    figs=(8,4.5)
+    plt.figure(figsize = figs)
 
-    return Response({'prices': prices_base64, 'spread': spread_base64, 'ratio': ratio_base64, 'bands': bands_base64, 'returns': returns_base64})
+    # process data
+    ticker_long = request.data.get('ticker1') or 'AAPL' 
+    ticker_short = request.data.get('ticker2') or 'MSFT' 
+    start_date_str = request.data.get('startDate')
+    end_date_str = request.data.get('endDate')
+    window = request.data.get('window') or 20
+    n_std = request.data.get('std') or 2
+
+    df_stocks = pd.DataFrame()
+    for ticker in [ticker_long, ticker_short]:
+        df_temp = download_stock_data(ticker, start_date_str, end_date_str)[['Date', 'Adj Close']]
+        df_temp = df_temp.set_index('Date')
+        df_temp.columns = [ticker]
+        df_stocks = pd.concat((df_stocks, df_temp), axis=1)
+
+    # cointegration and adfuller testing
+    coint_p, adf_p = coint_testing(df_stocks, ticker_long, ticker_short)
+    print('P value for the augmented Engle-Granger two-step cointegration test is', coint_p)
+    print('P value for the Augmented Dickey-Fuller Test is', adf_p)
+
+    ratio = df_stocks[ticker_long] / df_stocks[ticker_short]
+    zscore = (ratio - ratio.mean()) / ratio.std()
+    ratio_mean = ratio.mean()
+    ratio_std = ratio.std()
+    
+    ratio_rolling_mean = ratio.rolling(window=window).mean()
+    ratio_rolling_std = ratio.rolling(window=window).std()
+    rolling_zscore = (ratio - ratio_rolling_mean) / ratio_rolling_std
+
+    # plot stock prices
+    prices_base64 = plot_stock_prices(df_stocks, ticker_long, ticker_short)
+
+    # plot ratio with static
+    static_base64 = plot_ratio_static(ratio, ticker_long, ticker_short, n_std, ratio_mean, ratio_std)
+
+    # plot ratio with bands
+    bands_base64 = plot_ratio_bollinger(df_stocks, ratio, ticker_long, ticker_short, n_std, ratio_rolling_mean, ratio_rolling_std, window)
+
+    # plot static returns
+    static_returns_base64 = plot_returns(df_stocks, ticker_long, ticker_short, zscore, n_std)
+
+    # plot bands returns
+    bands_returns_base64 = plot_returns(df_stocks, ticker_long, ticker_short, rolling_zscore, n_std)
+
+    plt.close()
+
+    return Response({'prices': prices_base64, 'static': static_base64, 'bands': bands_base64, 'static_returns': static_returns_base64, 'bands_returns': bands_returns_base64})
