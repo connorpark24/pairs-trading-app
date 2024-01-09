0d8238d50851264905687919110414fb455ac477
Connor Park <connorpark024@gmail.com>
2024-01-09 16:55:26 -0500

    update django settings

--- server/stockproject/settings.py
+++ server/stockproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["pairs-trading-app.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["your_app_name.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "https://pairs-trading-ejk2dyg1v-connorpark24s-projects.vercel.app",
+
 ]
 
 
