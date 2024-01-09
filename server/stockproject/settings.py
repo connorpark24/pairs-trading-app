eabb17060c41884fc8bd2bceca48fdd9edcce70e
Connor Park <connorpark024@gmail.com>
2024-01-09 16:53:28 -0500

    create runtime.txt

--- server/stockproject/settings.py
+++ server/stockproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+kae@3n+!wb5qhns4%@3)m9(@%#=an=7i0!lz7tui!vpk820mk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pairs-trading-app.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
