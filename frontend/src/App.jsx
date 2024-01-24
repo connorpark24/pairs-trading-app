87e4a6ee14478d43e9b2bc08b5de5bce7b5cfb2a
Connor Park <connorpark024@gmail.com>
2024-01-23 22:01:13 -0500

    edit env variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -101,7 +101,7 @@ function App() {
     // }
 
     try {
-      const response = await axios.post(import.meta.env.VITE_API_URL, {
+      const response = await axios.post(import.meta.env.API_URL, {
         ticker1,
         ticker2,
         startDate,
